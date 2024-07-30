45ca794b477ad30d0573967af4428e1beb4a37df
JUNGHOON AN <gnstm831@naver.com>
2024-07-30 23:46:16 +0900

    For loop, Operators overloading, Invoke, Introduction
    Operators overloading(연산자 오버로딩) 추후 재진행 필요

--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -5,12 +5,14 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 // Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
